62bf9ddc58541148abb4f14cf593140e1096d8c6
Matheus Maia <matheusmaia590@gmail.com>
2021-06-18 00:16:12 -0300

    adicionando estilo

--- dreezer.js
+++ dreezer.js
@@ -8,7 +8,8 @@ const searchInput = document.querySelector('#inputSong');
 const buttonInput = document.querySelector('#btnSong');
 
 document.addEventListener('click', function(e){
-    if(e.target == buttonInput) search()
+    console.log(e.target);
+    if(e.target == buttonInput || e.target.parentElement == buttonInput || e.target.parentElement.parentElement == buttonInput) search()
 })
 
 
@@ -16,7 +17,8 @@ document.addEventListener('click', function(e){
 function search(){
     console.warn('iniciando busca');
     let data = searchInput.value;
-    result.innerHTML = 'Buscando'
+    result.classList.add("aligned");
+    result.innerHTML = '<div class="load"></div>'
 
     
     fetch(`https://deezerdevs-deezer.p.rapidapi.com/search?q=${data}`, {
@@ -38,26 +40,52 @@ function search(){
         })
         .then(musics => {
             result.innerHTML = '';
+            result.classList.remove("aligned");
+
 
             musics.forEach(music => {
+
+                
                 result.innerHTML += `
                 
                 <div class="music">
-                    <div class="album"><img src="${music.album.cover_small}"></div>
-                        <div class="title">
-                        ${music.title}
-                        </div>
+                    <div class="album">
+                        <img src="${music.album.cover_medium}">
+                            <div class="title">
+                               ${music.title}  <div class="artist"> by ${music.artist.name}</div>
+                            </div>
+                    </div>
                         <div class="audio">
-                        <audio src="${music.preview}" type="audio/mp3">
+                            <audio id="${music.id}" src="${music.preview}">
+                                Your browser does not support the audio element.
+                            </audio>
+                            <button onClick="play(${music.id})">
+                                <i class=" fas fa-solid fa-play"></i>
+                            </button>
                         </div>
                     </div>
         
                 </div>
         
                 `
+                
             });
+
+            
         })
     })
 
 
+}
+
+function play(musica){
+    const findAudio = `${musica}`
+    const audio = document.getElementById(findAudio);
+    if (audio.classList != 'playing' ) {
+        audio.classList.add("playing")
+        audio.play()
+    } else {
+        audio.pause()
+    }
+    console.log(audio);
 }
